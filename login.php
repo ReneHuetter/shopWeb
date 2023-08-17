a742b78902f331cf459855d44dee0df6cb4e3747
ReneHuetter <rene.huetter@gmx.at>
2023-08-17 13:59:11 +0200

    add login.php

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+require 'inc/head.php';
+include 'class/userClass.php';
+?>
+
+<form action="" method="post">
+    <h1>Login</h1>
+    <input type="text" name="username" placeholder="username" required>
+    <input type="password" name="pw" placeholder="Password" required>
+    <button type="submit" name="loginbtn">Login</button>
+</form>
+
+<?php
+if (isset($_POST['loginbtn'])) {
+    $user = new User();
+    $user->login($_POST['username'], sha1($_POST['pw']));
+}
+?>
+
+<?php
+require 'inc/footer.php'
+?>
