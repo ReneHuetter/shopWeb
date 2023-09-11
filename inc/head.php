abad98b7f11d1f7c23703513e080051775de9c8d
ReneHuetter <rene.huetter@gmx.at>
2023-09-11 09:53:14 +0200

    head fixet

--- inc/head.php
+++ inc/head.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css">
+    <link rel="stylesheet" href="/codersbay/shop/css/main.css" type="text/css">
+    <link rel="stylesheet" href="/codersbay/shop/css/nav.css" type="text/css">
     <title>Shop</title>
 </head>
 <body>
